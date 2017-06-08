58f97ae06a2df2623ba4ad0d747f4aed1c736691
pratherb7750 <pratherb7750@student.faytechcc.edu>
2017-06-08 12:10:22 -0400

    Add files via upload

---
+++ M2HW1_DistanceTraveled_BrucePrather.py
@@ -0,0 +1,24 @@
+# This program will display the distance a car travels based on MPH
+# June 7, 2017
+# CTI-110 M2HW1 - Distance Traveled 
+# Bruce Prather
+#
+#Distance = Speed X Time
+
+#Set speed varialble to 70 mph
+speed = 70
+
+#Get the amount of time the car has been traveling in hours (use decimal)
+time = float(input('How many hours has the car been travling? '))
+
+#Calculate the  distance
+distance = float(speed * time)
+
+print ('The car has traveled: ' , distance , 'miles.')
+
+
+
+
+
+
+
---
+++ M2T1_SalesPrediction_BrucePrather.py
@@ -0,0 +1,13 @@
+#This project is the Sales Prediction Algorithm
+#June 6, 2017
+#CTI-110 M2T1 - Sales Prediction
+#Bruce Prather
+#
+#Get the projected total sales.
+total_sales = float(input('Enter the projected sales: '))
+
+#Calculate the profit as 23 percent of the total sales.
+profit = total_sales * 0.23
+
+#Display the profit.
+print('The profit is $' , profit)
